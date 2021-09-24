5b52a415691ceb919a5a00545962b075fb739638
Xarakiri <rafikov.ds777@gmail.com>
2021-09-24 20:32:57 +0500

    add comment serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from api.models import Post
+from rest_framework.exceptions import server_error
+from api.models import Comment, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts']
+        fields = ['id', 'username', 'posts', 'comments']
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'owner']
+        fields = ['id', 'title', 'body', 'owner', 'comments']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'owner', 'post']
