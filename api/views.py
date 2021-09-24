7382a3dbb1cdfa4dc4753dfda71dba2b5803585e
Xarakiri <rafikov.ds777@gmail.com>
2021-09-24 20:06:48 +0500

    now only logged users able to create, delete or update any posts

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from api import serializers
 from django.contrib.auth.models import User
 from api.models import Post
+from api.permissions import IsOwnerOrReadOnly
 
 
 class UserList(generics.ListAPIView):
@@ -17,6 +18,7 @@ class UserDetail(generics.RetrieveAPIView):
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -25,6 +27,7 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
