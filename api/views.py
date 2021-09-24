b40ff1bbc3b0be027bd7e368aa1b7d601878e0e3
Xarakiri <rafikov.ds777@gmail.com>
2021-09-24 21:06:28 +0500

    add views for categories

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from api import serializers
 from django.contrib.auth.models import User
 from api.models import Post
 from api.models import Comment
+from api.models import Category
 from api.permissions import IsOwnerOrReadOnly
 
 
@@ -43,4 +44,19 @@ class CommentList(generics.ListCreateAPIView):
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
