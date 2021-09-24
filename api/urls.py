75d61f2aaa2772fbf0e1e4fba49a588388064f7b
Xarakiri <rafikov.ds777@gmail.com>
2021-09-24 23:06:17 +0500

    add tag to url patterns

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('comments/<int:pk>', views.CommentDetail.as_view()),
     path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('tags/', views.TagList.as_view()),
+    path('tags/<int:pk>', views.TagDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
